04d812c6d03ee6bd8576a71536a5bf10df744610
Shreyas Aithala <shreyasvaithala@gmail.com>
2018-01-02 17:21:15 +0530

    added methods for funtionaitess

--- calculator.java
+++ calculator.java
@@ -1,61 +1,78 @@
-package com.npntraining.practice;
-
+package LearnPackage;
 import java.util.Scanner;
 
 public class calculator {
 
-	static float a, b, res;
+	static float a,b,res;
+	static Scanner sc;
+
+	public static void addition() {
+		System.out.println("Enter two numbers");
+		a = sc.nextFloat();
+		b = sc.nextFloat();
+		res = a + b;
+		System.out.println("The result is " + res);
+	}
+
+	public static void subtraction() {
+		System.out.println("Enter two numbers");
+		a = sc.nextFloat();
+		b = sc.nextFloat();
+		res = a - b;
+		System.out.println("The result is " + res);
+	}
+
+	public static void multiplication() {
+		System.out.println("Enter two numbers");
+		a = sc.nextFloat();
+		b = sc.nextFloat();
+		res = a * b;
+		System.out.println("The result is " + res);
+	}
+
+	public static void division() {
+		System.out.println("Enter two numbers");
+		a = sc.nextFloat();
+		b = sc.nextFloat();
+		res = a / b;
+		System.out.println("The result is " + res);
+	}
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(System.in);
 		System.out.println("1: Addition");
-		System.out.println("2: Substraction");
+		System.out.println("2: Subtraction");
 		System.out.println("3: Multiplication");
 		System.out.println("4: Division");
-		System.out.println("Enter your choice");
+		System.out.println("Enter your choice");		
 		char result = sc.next().charAt(0);
-
+		
 		switch (result) {
 
 		case '1':
-			System.out.println("Enter two numbers");
-			a = sc.nextFloat();
-			b = sc.nextFloat();
-			res = a + b;
-			System.out.println("The result is " + res);
+			 addition();
 			break;
 
 		case '2':
-			System.out.println("Enter two numbers");
-			a = sc.nextFloat();
-			b = sc.nextFloat();
-			res = a - b;
-			System.out.println("The result is " + res);
+			subtraction();
 			break;
 
 		case '3':
-			System.out.println("Enter two numbers");
-			a = sc.nextFloat();
-			b = sc.nextFloat();
-			res = a * b;
-			System.out.println("The result is " + res);
+			multiplication();
 			break;
 
 		case '4':
-			System.out.println("Enter two numbers");
-			a = sc.nextFloat();
-			b = sc.nextFloat();
-			res = a / b;
-			System.out.println("The result is " + res);
+			division();
 			break;
 
 		case '5':
+			System.out.println("Wrong option");
 			System.exit(0);
 			break;
 
 		default:
 			System.out.println("Wrong option");
-
+			System.exit(0);
 		}
 		sc.close();
 	}
