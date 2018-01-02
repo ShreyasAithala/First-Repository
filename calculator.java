71a89fe09beff5dafc140998def2bde0cb142ab0
Shreyas Aithala <shreyasvaithala@gmail.com>
2018-01-02 17:57:23 +0530

    Updated the calculator code with switch repitation

--- calculator.java
+++ calculator.java
@@ -1,10 +1,12 @@
 package LearnPackage;
+
 import java.util.Scanner;
 
 public class calculator {
 
-	static float a,b,res;
+	static float a, b, res;
 	static Scanner sc;
+	static char flag;
 
 	public static void addition() {
 		System.out.println("Enter two numbers");
@@ -40,40 +42,46 @@ public class calculator {
 
 	public static void main(String[] args) {
 		sc = new Scanner(System.in);
-		System.out.println("1: Addition");
-		System.out.println("2: Subtraction");
-		System.out.println("3: Multiplication");
-		System.out.println("4: Division");
-		System.out.println("Enter your choice");		
-		char result = sc.next().charAt(0);
-		
-		switch (result) {
+		do {
+			System.out.println("1: Addition");
+			System.out.println("2: Subtraction");
+			System.out.println("3: Multiplication");
+			System.out.println("4: Division");
+			System.out.println("Enter your choice");
+			char result = sc.next().charAt(0);
 
-		case '1':
-			 addition();
-			break;
+			switch (result) {
+			case '1':
+				addition();
+				break;
 
-		case '2':
-			subtraction();
-			break;
+			case '2':
+				subtraction();
+				break;
 
-		case '3':
-			multiplication();
-			break;
+			case '3':
+				multiplication();
+				break;
 
-		case '4':
-			division();
-			break;
+			case '4':
+				division();
+				break;
 
-		case '5':
-			System.out.println("Wrong option");
-			System.exit(0);
-			break;
+			case '5':
+				System.out.println("Wrong option");
+				System.exit(0);
+				break;
 
-		default:
-			System.out.println("Wrong option");
-			System.exit(0);
-		}
+			default:
+				System.out.println("Wrong option");
+				System.exit(0);
+
+			}
+			System.out.println("Do you want to continue?");
+			flag = sc.next().charAt(0);
+		} while (flag == 'Y' || flag == 'y');
+
+		System.out.println("Thank you for using calculator");
 		sc.close();
 	}
 }
